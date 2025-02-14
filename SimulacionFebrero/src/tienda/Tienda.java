1378f676cfacbf41119dbcc1542ab56325dd7f4b
NereidaRO <nereidarodriguezorenes@gmail.com>
2025-02-14 15:58:58 +0100

    version 1.1 - menu da errores

--- SimulacionFebrero/src/tienda/Tienda.java
+++ SimulacionFebrero/src/tienda/Tienda.java
@@ -19,15 +19,32 @@ public class Tienda {
     public static void main(String[] args) {
         //Esta clase tiene el menú y llama a los diferentes métodos de Gestion
         //Atributos
+        int option;
         Scanner sc = new Scanner(System.in);
         boolean continuar = true;
         Gestion gestion = new Gestion();
         //Menú
-        gestion.introducirProducto();
-        gestion.mostrarArrayList();
-        gestion.mostrarCaja();
-        gestion.mostrarProducto();
-        gestion.venderProducto();
+        gestion.menu();
+        option = sc.nextInt();
+        sc.nextLine(); //los next de Scanner dejan un \n en el buffer; con esta línea se limpia
+        
+        switch (option){
+            case 1:
+                gestion.introducirProducto();
+                gestion.menu();
+            case 2:
+                gestion.mostrarProducto();
+                gestion.menu();
+            case 3:
+                gestion.venderProducto();
+                gestion.menu();
+            case 4:
+                gestion.mostrarCaja();
+                gestion.menu();
+            default:
+                System.out.println("Ha salido del programa");
+                break;
+        }
     }
     
 }

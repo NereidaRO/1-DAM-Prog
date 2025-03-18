7d7ee12357111568c09baaa606a738b3b37c9be9
NereidaRO <nereidarodriguezorenes@gmail.com>
2025-03-18 19:09:36 +0100

    added structure for byte and char methods

--- tratamientoDeFicheros/src/PCK_Byte/GestionFlujoBytes.java
+++ tratamientoDeFicheros/src/PCK_Byte/GestionFlujoBytes.java
@@ -9,5 +9,38 @@ package PCK_Byte;
  * @author Nereida Rodríguez Orenes 1ºDAM
  */
 public class GestionFlujoBytes {
-    
+    //Constantes
+    byte CR = 13;
+    byte LF = 10;
+    //Atributos
+    private String ruta;
+    //Constructores
+    public GestionFlujoBytes(String ruta) {
+        this.ruta = ruta;
+    }
+    //getters y setters
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+    //toString
+    @Override
+    public String toString() {
+        return "GestionFlujoBytes{" + "CR=" + CR + ", LF=" + LF + ", ruta=" + ruta + '}';
+    }
+    //Métodos de ejemplo
+    public void escribirBinario(String nombreFichero){}
+    public void escribirBinarioBuffer(String nombreFichero){}
+    public void escribirPrimitivo(String nombreFichero, int num){}
+    public void escribirPrimitivo(String nombreFichero, String text){}
+    public void escribirArray(String nombreFichero){}
+    public void imprimirByte(String nombreFichero){}
+    public void leerBinario(String nombreFichero){}
+    public void leerBinarioBuffer(String nombreFichero){}
+    public void leerPrimitivo(String nombreFichero){}
+    public void leerArrayByte(byte[] array){}
+    public void leerByteMultiple (){}
 }
--- tratamientoDeFicheros/src/PCK_Caracter/GestionFlujoCaracteres.java
+++ tratamientoDeFicheros/src/PCK_Caracter/GestionFlujoCaracteres.java
@@ -9,5 +9,34 @@ package PCK_Caracter;
  * @author Nereida Rodríguez Orenes 1ºDAM
  */
 public class GestionFlujoCaracteres {
+    private String ruta;
     
+    //Constructores
+    public GestionFlujoCaracteres(String ruta) {
+        this.ruta = ruta;
+    }   
+    //getters y setters
+    public String getRuta() {
+        return ruta;
+    }
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+    //toString
+    @Override
+    public String toString() {
+        return "GestionFlujoCaracteres{" + "ruta=" + ruta + '}';
+    }
+    
+    //Métodos de ejemplo
+    public void escribirCar(String nombreFichero){}
+    public void escribirCarBuffer(String nombreFichero){}
+    public void escribirCarArray(String nombreFichero){}
+    public void escribirCadena(){}
+    public void imprimirCar(){}
+    public void leerCar(String nombreFichero){}
+    public void leerCarBuffer(String nombreFichero){}
+    public void leerArrayCar(char[] array){}
+    public void leerLinea(String nombreFichero){}
+    public void leerString(String cadena){}
 }

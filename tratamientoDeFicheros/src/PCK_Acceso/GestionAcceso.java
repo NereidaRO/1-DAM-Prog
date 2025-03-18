1945956e0a2480109bc420def09f0afff4d99a3a
NereidaRO <nereidarodriguezorenes@gmail.com>
2025-03-18 17:53:18 +0100

    added package structure

---
+++ tratamientoDeFicheros/src/PCK_Acceso/GestionAcceso.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PCK_Acceso;
+
+/**
+ * @since 18/03/2025
+ * @author Nereida Rodríguez Orenes 1ºDAM
+ */
+public class GestionAcceso {
+    
+}
---
+++ tratamientoDeFicheros/src/PCK_Byte/GestionFlujoBytes.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PCK_Byte;
+
+/**
+ * @since 18/03/2025
+ * @author Nereida Rodríguez Orenes 1ºDAM
+ */
+public class GestionFlujoBytes {
+    
+}

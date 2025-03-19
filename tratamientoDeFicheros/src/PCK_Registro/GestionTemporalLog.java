4a3aeedd15872105cc7e7c3136eb341a0e0b48f2
NereidaRO <nereidarodriguezorenes@gmail.com>
2025-03-19 20:07:56 +0100

    logs v1.0

--- tratamientoDeFicheros/src/PCK_Registro/GestionTemporalLog.java
+++ tratamientoDeFicheros/src/PCK_Registro/GestionTemporalLog.java
@@ -4,11 +4,77 @@
  */
 package PCK_Registro;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  * @since 18/03/2025
  * @author Nereida Rodríguez Orenes 1ºDAM
- * Los objetos de esta clase manejarán los que se creen en la clase Registro.
+ * Los objetos de esta clase manejarán los que se creen en la clase Registro. Usar en este orden:
+ * 1 - constructor
+ * 2 - addToList();
+ * 3 - addTxt();
  */
 public class GestionTemporalLog {
+    private String ruta;
+    private ArrayList<Registro> registros;
+
+    //Constructor
+    public GestionTemporalLog(String ruta){
+        this.ruta = ruta;
+        this.registros = new ArrayList<>();
+    }
     
+    //getters y setters
+    public String getRuta() {
+        return ruta;
+    }
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+    
+    //gestión de registros
+    public void addToList(int id, String nombre){
+        //crear registro
+        LocalDateTime fecha = LocalDateTime.now();
+        Registro reg = new Registro(id, nombre, fecha);
+        System.out.println("Registro creado");
+        //añadir objeto Registro a registros
+        this.registros.add(reg);
+        System.out.println("Registro añadido a la lista");
+    }
+    
+    public void addToList(Registro reg){
+        this.registros.add(reg);
+        System.out.println("Registro añadido a la lista");
+    }
+
+    public void addTxt(String nombreArchivo){
+        //añadir los registros del array al txt creado antes
+        if(registros.isEmpty()){
+            System.out.println("No tienes registros");
+        }else{
+            Iterator it = registros.iterator();
+            try{
+                //de esta manera crea el archivo.txt
+                FileWriter fw = new FileWriter(ruta + nombreArchivo);
+                BufferedWriter bw = new BufferedWriter(fw);
+                while(it.hasNext()){
+                    //pasar a String y meter en buffer
+                    String regi = it.next().toString();
+                    System.out.println(regi);
+                    bw.write(regi);
+                    bw.newLine();
+                }
+                bw.close();
+                fw.close();
+            }catch(IOException ioe){
+                System.out.println(ioe);
+            }
+        }
+    }
 }
--- tratamientoDeFicheros/src/PCK_Registro/Registro.java
+++ tratamientoDeFicheros/src/PCK_Registro/Registro.java
@@ -4,10 +4,50 @@
  */
 package PCK_Registro;
 
+import java.time.*;
+
 /**
- *
- * @author Asus
+ * @since 18/03/2025
+ * @author Nereida Rodríguez Orenes 1ºDAM
+ * Los objetos de esta clase representarán registros sencillos con fecha, para trabajar con fechas.
+ * Como java.util.Date está obsoleta para la mayoría de casos, usaré java.time.*
  */
 public class Registro {
-    
+    private int id;
+    private String nombre;
+    private LocalDateTime fechaHora;
+
+    //En este caso, todos los campos son obligatorios, puesto que no tiene sentido un registro sin ellos
+    public Registro(int id, String nombre, LocalDateTime fechaHora) {
+        this.id = id;
+        this.nombre = nombre;
+        this.fechaHora = fechaHora;
+    }
+
+    //Getters y setters
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(LocalDateTime fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    //Esto es importante: hay que meter estos strings a un documento de registros
+    @Override
+    public String toString() {
+        return "Registro{" + "id=" + id + ", nombre=" + nombre + ", fechaHora=" + fechaHora + '}';
+    }
 }
